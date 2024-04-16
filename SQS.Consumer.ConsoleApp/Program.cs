ed6cab04019c1cdf97a6b8efd00f747fddf2e215
Emre Can Topaloğlu <emrecantopaloglu@gmail.com>
2024-04-16 15:52:05 +0300

    Add project files.

---
+++ SQS.Consumer.ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+
+var sqsClient = new AmazonSQSClient();
+
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers");
+
+var receiveMessageRequest = new ReceiveMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    AttributeNames = new List<string>() { "All" },
+    MessageAttributeNames = new List<string>() { "All" }
+};
+
+var cts = new CancellationTokenSource(); //sonsuz döngüde kalmasın diye best-practice'i cancellationToken
+
+while (!cts.IsCancellationRequested)
+{
+    var response = await sqsClient.ReceiveMessageAsync(receiveMessageRequest);
+    foreach (var message in response.Messages)
+    {
+        //Mail gönder
+        //SMS gönder
+
+        try
+        {
+            Console.WriteLine($"Message Id: {message.MessageId}");
+            Console.WriteLine($"Message Body: {message.Body}");
+
+            await sqsClient.DeleteMessageAsync(queueUrlResponse.QueueUrl, message.ReceiptHandle); //kuyruktan kaldırıyorum, işlem başarılıysa.
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+
+    await Task.Delay(100);
+}
---
+++ SQS.Publisher.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.Model;
+using System.Text.Json;
+
+//string accessKey = string.Empty;
+//string secretKey = string.Empty;
+//var region = Amazon.RegionEndpoint.EUWest3;
+
+var sqsClient = new AmazonSQSClient();
+
+var customer = new
+{
+    FirstName = "Emre",
+    LastName = "Can",
+    Age = 28
+};
+
+Console.WriteLine(" [*] Started sending queue message...");
+var queueUrlResponse = await sqsClient.GetQueueUrlAsync("Customers"); //kuyruk ismi ile bağlanmak için
+
+var sendMessageRequest = new SendMessageRequest
+{
+    QueueUrl = queueUrlResponse.QueueUrl,
+    MessageBody = JsonSerializer.Serialize(customer), //Kuyruğa sadece string değer ekleyebiliyoruz.
+    MessageAttributes = new Dictionary<string, MessageAttributeValue>
+    {
+        {
+            "MessageType", new MessageAttributeValue
+            {
+                DataType = "String",
+                StringValue = "Customer"
+            }
+        }
+    }
+};
+
+SendMessageResponse response = await sqsClient.SendMessageAsync(sendMessageRequest);
+
+Console.WriteLine(" [*] Sending queue message is now complete...");
+Console.Read();
+
+
